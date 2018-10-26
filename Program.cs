75e0edae7d9042d045f27755b6554a7e1b8a4622
Tangram ($TGM) <dev@getsneak.org>
2018-10-26 23:45:07 +0400

    Added logging

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using TangramCypher.ApplicationLayer.Actor;
 using TangramCypher.ApplicationLayer.Wallet;
 using TangramCypher.Helpers.ServiceLocator;
 using TangramCypher.Helpers.LibSodium;
+using Microsoft.Extensions.Logging;
 
 namespace TangramCypher
 {
@@ -17,9 +17,16 @@ namespace TangramCypher
             var serviceCollection = new ServiceCollection();
             ConfigureServices(serviceCollection);
 
-            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var serviceProvider = serviceCollection
+                .AddLogging()
+                .BuildServiceProvider();
+
+
+            var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
 
             locator.Add<IServiceProvider, ServiceProvider>(serviceProvider);
+
+            logger.LogDebug("Starting application");
 
         }
 
