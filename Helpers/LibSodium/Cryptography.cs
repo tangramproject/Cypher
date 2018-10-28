dd5f548dd8b62f76a1d2bfff3b491b01b3d4b0a7
Tangram ($TGM) <dev@getsneak.org>
2018-10-29 00:55:54 +0400

    Added null checks.

--- Helpers/LibSodium/Cryptography.cs
+++ Helpers/LibSodium/Cryptography.cs
@@ -7,17 +7,37 @@ namespace TangramCypher.Helpers.LibSodium
     {
         public byte[] BoxSeal(string message, byte[] pk)
         {
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new System.ArgumentException("message", nameof(message));
+            }
+
+            if (pk == null)
+            {
+                throw new System.ArgumentNullException(nameof(pk));
+            }
+
             var encrypted = SealedPublicKeyBox.Create(Encoding.UTF8.GetBytes(message), pk);
             return encrypted;
         }
 
         public byte[] GenericHash(string message, int bytes = 32)
         {
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new System.ArgumentException("Message cannot be null or empty!", nameof(message));
+            }
+
             return Sodium.GenericHash.Hash(Encoding.UTF8.GetBytes(message), null, bytes);
         }
 
         public byte[] HashPwd(string pwd)
         {
+            if (string.IsNullOrEmpty(pwd))
+            {
+                throw new System.ArgumentException("Password cannot be null or empty!", nameof(pwd));
+            }
+
             const long OPS_LIMIT = 4;
             const int MEM_LIMIT = 33554432;
 
@@ -34,6 +54,16 @@ namespace TangramCypher.Helpers.LibSodium
 
         public string OpenBoxSeal(byte[] cipher, KeyPair keyPair)
         {
+            if (cipher == null)
+            {
+                throw new System.ArgumentNullException(nameof(cipher));
+            }
+
+            if (keyPair == null)
+            {
+                throw new System.ArgumentNullException(nameof(keyPair));
+            }
+
             var decrypted = SealedPublicKeyBox.Open(cipher, keyPair);
             return Encoding.UTF8.GetString(decrypted);
         }
@@ -45,16 +75,41 @@ namespace TangramCypher.Helpers.LibSodium
 
         public byte[] ScalarMultBase(byte[] sk)
         {
+            if (sk == null)
+            {
+                throw new System.ArgumentNullException(nameof(sk));
+            }
+
             return Sodium.ScalarMult.Base(sk);
         }
 
         public byte[] ScalarMult(byte[] bobSk, byte[] alicePk)
         {
+            if (bobSk == null)
+            {
+                throw new System.ArgumentNullException(nameof(bobSk));
+            }
+
+            if (alicePk == null)
+            {
+                throw new System.ArgumentNullException(nameof(alicePk));
+            }
+
             return Sodium.ScalarMult.Mult(bobSk, alicePk);
         }
 
         public bool VerifiyPwd(byte[] hash, byte[] pwd)
         {
+            if (hash == null)
+            {
+                throw new System.ArgumentNullException(nameof(hash));
+            }
+
+            if (pwd == null)
+            {
+                throw new System.ArgumentNullException(nameof(pwd));
+            }
+
             return PasswordHash.ArgonHashStringVerify(hash, pwd);
         }
     }
