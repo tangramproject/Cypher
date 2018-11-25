f9042dd9833e478b7e40327bdee4549f7a50bb33
Tangram ($TGM) <dev@getsneak.org>
2018-11-25 02:14:33 +0200

    Added Api exception class,

---
+++ Helpers/ApiException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TangramCypher.Helpers
+{
+    public class ApiException: Exception
+    {
+        public int StatusCode { get; set; }
+
+        public string Content { get; set; }
+    }
+}
