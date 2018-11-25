588eee09f506bc1cbcabdd2683edc1b285dd6d73
Tangram ($TGM) <dev@getsneak.org>
2018-11-25 03:00:09 +0200

    Added token dto.

---
+++ ApplicationLayer/Actor/TokenDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TangramCypher.ApplicationLayer.Actor
+{
+    public class TokenDto
+    {
+        public EnvelopeDto Envelope { get; set; }
+
+        public string Hint { get; set; }
+
+        public string Keeper { get; set; }
+
+        public int Version { get; set; }
+
+        public string Principle { get; set; }
+
+        public string Stamp { get; set; }
+    }
+}
