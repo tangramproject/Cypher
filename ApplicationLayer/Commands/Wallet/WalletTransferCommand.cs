63251facd3feba00b6c5199676542d23854fe8d5
Tangram ($TGM) <dev@getsneak.org>
2018-12-03 05:27:56 +0200

    Added initial transfer funds.

--- ApplicationLayer/Commands/Wallet/WalletTransferCommand.cs
+++ ApplicationLayer/Commands/Wallet/WalletTransferCommand.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using TangramCypher.ApplicationLayer.Vault;
+using TangramCypher.Helpers.ServiceLocator;
+using Microsoft.Extensions.DependencyInjection;
+using TangramCypher.ApplicationLayer.Actor;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TangramCypher.ApplicationLayer.Commands.Wallet
 {
     [CommandDescriptor(new string[] { "wallet", "transfer" }, "Transfer funds")]
     public class WalletTransferCommand : Command
     {
-        public override Task Execute()
+        readonly IActorService actorService;
+        readonly IVaultService vaultService;
+        readonly IConsole console;
+
+        public WalletTransferCommand()
         {
-            throw new NotImplementedException();
+            var serviceProvider = Locator.Instance.GetService<IServiceProvider>();
+
+            actorService = serviceProvider.GetService<IActorService>();
+            vaultService = serviceProvider.GetService<IVaultService>();
+            console = serviceProvider.GetService<IConsole>();
+        }
+
+        public override async Task Execute()
+        {
+            var identifier = Prompt.GetPassword("Identifier:", ConsoleColor.Yellow);
+            var password = Prompt.GetPassword("Password:", ConsoleColor.Yellow);
+            var amount = Prompt.GetString("Amount:", null, ConsoleColor.Red);
+            var address = Prompt.GetString("To:", null, ConsoleColor.Green);
+
+            var data = await vaultService.GetDataAsync(identifier, password, $"wallets/{identifier}/wallet");
+            var stroreKeys = JObject.FromObject(data.Data["storeKeys"]);
+
+            actorService
+                .From(password)
+                .Secret(stroreKeys.GetValue("SecretKey").Value<string>())
+                .Amount(Convert.ToDouble(amount))
+                .To(address)
+                .SendPayment();
         }
     }
 }
