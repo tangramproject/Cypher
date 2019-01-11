147df3ab17adfa03c2f5bd7cd5a1b656357674f5
pingpongsneak <pingpong@getsneak.org>
2019-01-11 16:01:28 +0200

    Added Api gateway n-route.

---
+++ ApplicationLayer/Actor/Constant.cs
@@ -0,0 +1,19 @@
+﻿namespace TangramCypher.ApplicationLayer.Actor
+{
+    public static class Constant
+    {
+        public const string ApiGateway = "api_gateway";
+        public const string Endpoint = "endpoint";
+        public const string Routing = "routing";
+        public const string PostBlock = "postBlock";
+        public const string GetBlockMerkle = "getBlockMerkle";
+        public const string PostCoin = "postCoin";
+        public const string GetCoinStamp = "getCoinStamp";
+        public const string GetTransaction = "getTransaction";
+        public const string GetTransactionCount = "getTransactionCount";
+        public const string GetTransactionRange = "getTransactionRange";
+        public const string GetVerifiyShortTransactions = "getVerifiyShortTransactions";
+        public const string GetMessage = "getMessage";
+        public const string PostMessage = "postMessage";
+    }
+}
