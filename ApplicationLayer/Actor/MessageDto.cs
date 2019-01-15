a5f8700c9b059a72ba13a061e81728c19e5e1dd1
pingpongsneak <pingpong@getsneak.org>
2019-01-15 02:48:08 +0200

    Added Message Dto.

---
+++ ApplicationLayer/Actor/MessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TangramCypher.ApplicationLayer.Actor
+{
+    public class MessageDto
+    {
+        public string Address { get; set; }
+        public string Body { get; set; }
+    }
+}
