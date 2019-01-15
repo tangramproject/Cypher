4ef8338ed78cdb99eb67571b0f5d44efa42cee72
pingpongsneak <pingpong@getsneak.org>
2019-01-15 02:48:31 +0200

    Added helper function.

--- Helpers/ExtentionMethods.cs
+++ Helpers/ExtentionMethods.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
 using Newtonsoft.Json;
+using Sodium;
 
 namespace TangramCypher.Helpers
 {
@@ -13,9 +14,10 @@ namespace TangramCypher.Helpers
     {
         public static StringContent AsJson(this object o)
           => new StringContent(JsonConvert.SerializeObject(o), Encoding.UTF8, "application/json");
-        public static string ToHex(this byte[] data) => Sodium.Utilities.BinaryToHex(data);
-        public static byte[] FromHex(this string hex) => Sodium.Utilities.HexToBinary(hex);
-        public static byte[] ToByteArrayWithPadding(this String str)
+        public static string ToHex(this byte[] data) => Utilities.BinaryToHex(data);
+        public static byte[] FromHex(this string hex) => Utilities.HexToBinary(hex);
+        public static string ToBase64(this byte[] data) => Convert.ToBase64String(Encoding.UTF8.GetBytes(Utilities.BinaryToHex(data)));
+        public static byte[] ToByteArrayWithPadding(this string str)
         {
             const int BlockingSize = 16;
             int byteLength = ((str.Length / BlockingSize) + 1) * BlockingSize;
@@ -59,6 +61,35 @@ namespace TangramCypher.Helpers
             finally
             {
                 Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+        public static T ToArray<T>(this SecureString src, Func<byte[], T> func)
+        {
+            IntPtr bstr = IntPtr.Zero;
+            byte[] workArray = null;
+            GCHandle handle = GCHandle.Alloc(workArray, GCHandleType.Pinned);
+            try
+            {
+                bstr = Marshal.SecureStringToBSTR(src);
+                unsafe
+                {
+                    byte* bstrBytes = (byte*)bstr;
+                    workArray = new byte[src.Length * 2];
+
+                    for (int i = 0; i < workArray.Length; i++)
+                        workArray[i] = *bstrBytes++;
+                }
+
+                return func(workArray);
+            }
+            finally
+            {
+                if (workArray != null)
+                    for (int i = 0; i < workArray.Length; i++)
+                        workArray[i] = 0;
+                handle.Free();
+                if (bstr != IntPtr.Zero)
+                    Marshal.ZeroFreeBSTR(bstr);
             }
         }
     }
