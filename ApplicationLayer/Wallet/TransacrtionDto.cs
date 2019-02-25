66e0f5b7af9f5898ef4de70f4e814e40dfc8f36a
pingpongsneak <pingpong@getsneak.org>
2019-02-25 10:26:14 +0200

    Added missing class files.

---
+++ ApplicationLayer/Wallet/TransacrtionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TangramCypher.ApplicationLayer.Wallet
+{
+    public class TransacrtionDto
+    {
+        public double Amount { get; set; }
+        public string Commitment { get; set; }
+        public string Hash { get; set; }
+        public string Stamp { get; set; }
+        public int Version { get; set; }
+    }
+
+}
---
+++ ApplicationLayer/Wallet/TransactionChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TangramCypher.ApplicationLayer.Actor;
+
+namespace TangramCypher.ApplicationLayer.Wallet
+{
+    public class TransactionChange
+    {
+        public List<TransactionDto> Transactions { get; set; } = new List<TransactionDto>();
+        public TransactionDto Transaction { get; set; }
+        public double AmountFor { get; set; }
+    }
+}
