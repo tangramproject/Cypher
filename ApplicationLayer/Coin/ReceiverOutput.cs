6644a5b472cb48a7a0d9e8cf8425442994f00bdb
pingpongsneak <pingpong@getsneak.org>
2019-02-26 01:23:16 +0200

    Added receiver output class.

---
+++ ApplicationLayer/Coin/ReceiverOutput.cs
@@ -0,0 +1,19 @@
+﻿namespace TangramCypher.ApplicationLayer.Coin
+{
+    public partial class CoinService
+    {
+        public class ReceiverOutput
+        {
+            public double Amount { get; private set; }
+            public byte[] Commit { get; private set; }
+            public byte[] Blind { get; private set; }
+
+            public ReceiverOutput(double amount, byte[] commit, byte[] blind)
+            {
+                Amount = amount;
+                Commit = commit;
+                Blind = blind;
+            }
+        };
+    }
+}
