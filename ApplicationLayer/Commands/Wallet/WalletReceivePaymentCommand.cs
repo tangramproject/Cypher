73bc87b84fc529abbee8352aa7694e80a25a72db
pingpongsneak <pingpong@getsneak.org>
2019-03-04 03:05:24 +0200

    Added missing files.

---
+++ ApplicationLayer/Commands/Wallet/WalletReceivePaymentCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using TangramCypher.ApplicationLayer.Vault;
+using Microsoft.Extensions.DependencyInjection;
+using TangramCypher.ApplicationLayer.Actor;
+using TangramCypher.Helper;
+using TangramCypher.ApplicationLayer.Wallet;
+using TangramCypher.Helper.LibSodium;
+using System.Collections.Generic;
+
+namespace TangramCypher.ApplicationLayer.Commands.Wallet
+{
+    [CommandDescriptor(new string[] { "wallet", "receive" }, "Receive payment")]
+    public class WalletReceivePaymentCommand : Command
+    {
+        readonly IActorService actorService;
+        readonly IConsole console;
+        readonly IVaultService vaultService;
+        readonly IWalletService walletService;
+
+        public WalletReceivePaymentCommand(IServiceProvider serviceProvider)
+        {
+            actorService = serviceProvider.GetService<IActorService>();
+            console = serviceProvider.GetService<IConsole>();
+            vaultService = serviceProvider.GetService<IVaultService>();
+            walletService = serviceProvider.GetService<IWalletService>();
+        }
+
+        public override async Task Execute()
+        {
+            try
+            {
+                var identifier = Prompt.GetPassword("Identifier:", ConsoleColor.Yellow).ToSecureString();
+                var password = Prompt.GetPassword("Password:", ConsoleColor.Yellow).ToSecureString();
+                var address = Prompt.GetString("Address:", null, ConsoleColor.Red);
+
+                using (var insecureIdentifier = identifier.Insecure())
+                using (var insecurePassword = password.Insecure())
+                {
+                    await vaultService.GetDataAsync(insecureIdentifier.Value, insecurePassword.Value, $"wallets/{insecureIdentifier.Value}/wallet");
+                }
+
+                if (!string.IsNullOrEmpty(address))
+                {
+                    await actorService
+                      .From(password)
+                      .Identifier(identifier)
+                      .ReceivePayment(address);
+
+                    var total = await walletService.AvailableBalance(identifier, password);
+
+                    console.ForegroundColor = ConsoleColor.Magenta;
+                    console.WriteLine($"\nWallet balance: {total}\n");
+                    console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ApplicationLayer/Wallet/MessageTrackDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TangramCypher.ApplicationLayer.Wallet
+{
+    public class MessageTrackDto
+    {
+        public int Count { get; set; }
+        public string PublicKey { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
