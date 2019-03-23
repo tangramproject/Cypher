01934ad39908e90ba81d4e7d126991d1b3a0938d
pingpongsneak <pingpong@getsneak.org>
2019-03-23 18:14:22 +0200

    Added shell progress bar.

---
+++ Helper/ShellProgress/Styling.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ShellProgressBar;
+
+namespace TangramCypher.Helper.ShellProgress
+{
+    public class Styling: ProgressBarBase
+    {
+        readonly string message;
+        readonly int totalTicks;
+
+        public Styling(string message, int totalTicks)
+        {
+            this.message = message;
+            this.totalTicks = totalTicks;
+        }
+
+        protected override void Start()
+        {
+            var options = new ProgressBarOptions
+            {
+                ForegroundColor = ConsoleColor.Yellow,
+                ForegroundColorDone = ConsoleColor.DarkGreen,
+                BackgroundColor = ConsoleColor.DarkGray,
+                BackgroundCharacter = '\u2593'
+            };
+            using (var pbar = new ProgressBar(totalTicks, message, options))
+            {
+                TickToCompletion(pbar, totalTicks, sleep: 500);
+            }
+        }
+    }
+}
