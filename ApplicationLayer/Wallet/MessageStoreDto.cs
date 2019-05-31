0ff64e277b7961d2a8c80b8903a6a24c09bebe35
pingpongsneak <pingpong@getsneak.org>
2019-05-31 21:13:06 +0200

    Added message store backing.

---
+++ ApplicationLayer/Wallet/MessageStoreDto.cs
@@ -0,0 +1,22 @@
+// Cypher (c) by Tangram Inc
+// 
+// Cypher is licensed under a
+// Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+// 
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+
+using System;
+using TangramCypher.ApplicationLayer.Actor;
+
+namespace TangramCypher.ApplicationLayer.Wallet
+{
+    public class MessageStoreDto
+    {
+        public DateTime DateTime { get; set; }
+        public string Hash { get; set; }
+        public string PublicKey { get; set; }
+        public MessageDto Message { get; set; }
+        public string Memo { get; set; }
+    }
+}
