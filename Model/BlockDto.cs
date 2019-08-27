924e5e64469b539af21e12d713da0450e4026f9e
pingpongsneak <pingpong@getsneak.org>
2019-08-27 12:37:03 +0200

    Adding members to blockID Dto.

--- Model/BlockDto.cs
+++ Model/BlockDto.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿// Cypher (c) by Tangram Inc
+// 
+// Cypher is licensed under a
+// Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+// 
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+
+using ProtoBuf;
+
 namespace TangramCypher.Model
 {
+    [ProtoContract]
     public class BlockDto
     {
-        public BlockDto()
-        {
-        }
+        [ProtoMember(1)]
+        public string Address { get; set; }
+        [ProtoMember(2)]
+        public string BlockHash { get; set; }
+        [ProtoMember(3)]
+        public BaseCoinDto Coin { get; set; }
+        [ProtoMember(4)]
+        public string CoinHash { get; set; }
+        [ProtoMember(5)]
+        public string PublicKey { get; set; }
+        [ProtoMember(6)]
+        public string Signature { get; set; }
     }
 }
--- Model/BlockIDDto.cs
+++ Model/BlockIDDto.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿// Cypher (c) by Tangram Inc
+// 
+// Cypher is licensed under a
+// Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+// 
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+
+using ProtoBuf;
+
 namespace TangramCypher.Model
 {
+    [ProtoContract]
     public class BlockIDDto
     {
-        public BlockIDDto()
-        {
-        }
+        [ProtoMember(1)]
+        public string Hash;
+        [ProtoMember(2)]
+        public ulong Node;
+        [ProtoMember(3)]
+        public ulong Round;
+        [ProtoMember(4)]
+        public BlockDto SignedBlock;
     }
 }
