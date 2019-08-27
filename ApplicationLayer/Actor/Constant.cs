324ffb65c64083972b936dbac636a01fe146be7a
pingpongsneak <pingpong@getsneak.org>
2019-08-27 22:11:31 +0200

    Removing depreciated rest api endpoints.

--- ApplicationLayer/Actor/Constant.cs
+++ ApplicationLayer/Actor/Constant.cs
@@ -15,15 +15,9 @@ namespace TangramCypher.ApplicationLayer.Actor
         public const string ApiGateway = "api_gateway";
         public const string Endpoint = "endpoint";
         public const string Routing = "routing";
-        public const string PostBlock = "postBlock";
-        public const string GetBlockMerkle = "getBlockMerkle";
         public const string PostCoin = "postCoin";
         public const string GetCoin = "getCoin";
         public const string GetCoins = "getCoins";
-        public const string GetTransaction = "getTransaction";
-        public const string GetTransactionCount = "getTransactionCount";
-        public const string GetTransactionRange = "getTransactionRange";
-        public const string GetVerifiyShortTransactions = "getVerifiyShortTransactions";
         public const string GetMessage = "getMessage";
         public const string PostMessage = "postMessage";
         public const string GetMessages = "getMessages";
